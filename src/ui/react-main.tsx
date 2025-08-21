547bf31fc71d57ef9adab401e113531bf137b919
Kevin Klatt <klattkev@gmail.com>
2025-08-21 17:31:11 +0700

    analytics

--- src/ui/react-main.tsx
+++ src/ui/react-main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./Main";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(
   document.getElementById("react-root") as HTMLElement
 ).render(
   <React.StrictMode>
+    <Analytics />
     <Main />
   </React.StrictMode>
 );
