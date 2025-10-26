7a5194924d96ba14a32e29be663afd85a1d67da0
Kevin Klatt <klattkev@gmail.com>
2025-10-26 03:36:16 +0100

    server url

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,11 +2,14 @@ export async function handleGoogleLogin(token?: string): Promise<boolean> {
   if (!token) return false;
 
   try {
-    const res = await fetch("http://localhost:3000/auth/google", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ token }),
-    });
+    const res = await fetch(
+      `${(import.meta as any).env.VITE_SERVER_URL}/auth/google`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token }),
+      }
+    );
 
     if (!res.ok) return false;
 
@@ -22,12 +25,15 @@ export async function handleValidateJWT(token?: string): Promise<boolean> {
   if (!token) return false;
 
   try {
-    const res = await fetch("http://localhost:3000/auth/validate", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const res = await fetch(
+      `${(import.meta as any).env.VITE_SERVER_URL}/auth/validate`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
     return res.ok; // true if 200, false if 401/403
   } catch (err) {
